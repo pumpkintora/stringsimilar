dfd7aa0e3de81a982e316ff37029784699ef0f4a
pumpkintora <renkiat.gan@corpxervices.com>
2023-04-13 22:39:09 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var stringSimilarity = require("string-similarity");
+
+var similarity = stringSimilarity.compareTwoStrings("healed", "sealed");
+
+var matches = stringSimilarity.findBestMatch("healed", [
+  "edward",
+  "sealed",
+  "theatre",
+]);
+console.log(similarity, matches)
